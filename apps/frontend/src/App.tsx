bca0d2e0725f5ecda2b8f8318680a2ed8da725e8
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2025-03-21 00:49:24 +0200

    added page for 3d model viewer

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer"
 import HomePage from "@/components/HomePage"
 import PrintablePage from "./components/PrintablePage"
 import UploadPage from "./components/UploadPage"
+import ModelViewer from "./components/ModelViewer"
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/printable/:id" element={<PrintablePage />} />
             <Route path="/upload" element={<UploadPage />} />
+            <Route path="/model" element={<ModelViewer/>} />
           </Routes>
         <Footer />
     </Router>
