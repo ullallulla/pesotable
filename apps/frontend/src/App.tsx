a0a5b5cb57e8aea926da4253976abcd56c558487
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2025-03-30 00:42:54 +0200

    fixed single print model view

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -6,7 +6,7 @@ import PrintablePage from './components/PrintablePage';
 import UploadPage from './components/UploadPage';
 import ModelViewer from './components/ModelViewer';
 import PostHogPageView from './lib/pageview';
-// import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from './components/ScrollToTop';
 import AdminPanel from './components/AdminPanel';
 import ModelsAdminPage from './components/ModelsAdminPage';
 import NewModelPage from './components/NewModelPage';
@@ -16,10 +16,10 @@ const App = () => {
         <Router>
             <PostHogPageView />
             <Header />
-            {/* <ScrollToTop> */}
+            <ScrollToTop>
                 <Routes>
                     <Route path='/' element={<HomePage />} />
-                    <Route path='/printable/:id' element={<PrintablePage />} />
+                    <Route path='/printModel/:id' element={<PrintablePage />} />
                     <Route path='/upload' element={<UploadPage />} />
                     <Route path='/model' element={<ModelViewer />} />
 
@@ -29,7 +29,7 @@ const App = () => {
                         <Route path="/admin/models/new" element={<NewModelPage />} />
                     </Route>
                 </Routes>
-            {/* </ScrollToTop> */}
+            </ScrollToTop>
             <Footer />
         </Router>
     );
